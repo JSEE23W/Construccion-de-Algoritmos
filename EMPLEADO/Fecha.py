ee9f4e3f701eaf80864c7359c6d1b06872a0024d
JSEE23W <160885163+JSEE23W@users.noreply.github.com>
2024-03-05 14:24:40 -0500

    Update Fecha.py

--- EMPLEADO/Fecha.py
+++ EMPLEADO/Fecha.py
@@ -2,10 +2,10 @@ class Fecha:
     """-----------------------------------------------
     # Atributos
     -----------------------------------------------"""
-
     dia=0
     mes=0
     anio=0
+    
  def ConsultararDia(self):
     self.dia
 def ConsultarMes(self):
